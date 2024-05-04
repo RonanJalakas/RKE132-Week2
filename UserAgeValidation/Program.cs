108dfd41e23018813a9c73a3709d50722a930f50
RonanJalakas <ronan.jalakas@tktk.ee>
2024-05-04 11:21:21 +0300

    create new app

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.WriteLine("please, select your gender (m/f:");
+
+char  userGender = Char .Parse (Console.ReadLine());
+Console.WriteLine("Pleas, enter your last name:");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr.{userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+ 
+
+
+

8bea3cea1429db1d9b951a55a844dd8901e642e2
RonanJalakas <ronan.jalakas@tktk.ee>
2024-05-04 11:23:41 +0300

    Update Program.cs

--- Week2/Program.cs
+++ Week2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("please, select your gender (m/f:");
+
+char  userGender = Char .Parse (Console.ReadLine());
+Console.WriteLine("Pleas, enter your last name:");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr.{userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+ 
+
+
+
